193d866143532ed13ae8543f72c7401a87130830
SickSect <sleepyboy906sb@gmail.com>
2022-03-12 18:50:34 +0300

    qwe

--- swap.c
+++ swap.c
@@ -1,35 +1,5 @@
 #include "push_swap.h"
 
-/*
-struct s_node *f_sa(t_node *head, t_node *next)
-{
-    if (!head)
-        return (NULL);
-    if (!next)
-        return (head);
-    head->p_next = next->p_next;
-    next->p_next = head;
-    return (next);
-}
-
-struct s_node *f_sb(t_node *head, t_node *next)
-{
-    if (!head)
-        return (NULL);
-    if (!next)
-        return (head);
-    head->p_next = next->p_next;
-    next->p_next = head;
-    return (next);
-}
-
-void f_ss(t_node *head_a, t_node *head_b)
-{
-    head_a = f_sa(head_a,head_a->p_next);
-    head_b = f_sb(head_b, head_b->p_next);
-}
-*/
-
 void ft_sa(t_node **head)
 {
 	t_node *next;
@@ -59,4 +29,9 @@ void ft_sb(t_node **head);
 	(*head)->p_next = next->p_next;
 	next->p_next = (*head);
 	*head = next;
+}
+
+void ft_ss(t_node **head_a, t_node **head_b)
+{
+	ft_sa()
 }
